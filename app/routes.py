3a6b766c35adc3d26887695288437279c2a29918
Betobaz <pedroalberto007@gmail.com>
2013-04-23 09:17:18 -0500

    Agregando el formulario de contacto

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from forms import ContactForm
 
 app = Flask(__name__)
+app.secret_key = 'development key'
 
 @app.route('/')
 def home():
@@ -9,6 +11,15 @@ def home():
 @app.route('/about')
 def about():
 	return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+	form = ContactForm()
+
+	if request.method == 'POST':
+		return 'Form posted.'
+	elif request.method == 'GET':
+		return render_template('contact.html', form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
